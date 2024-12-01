f0bf45c964ef71ffc9bdffb5efedff9a39c1e7a3
MehdiiAH <mehdiait94@gmx.fr>
2024-12-01 16:58:32 +0100

    day1

---
+++ day1/day1.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+def read_and_split_numbers(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    left_list = []
+    right_list = []
+    for line in lines:
+        left, right = map(int, line.split())
+        left_list.append(left)
+        right_list.append(right)
+    return left_list, right_list
+
+def calculate_total_distance(file_path):
+    left_list, right_list = read_and_split_numbers(file_path)
+    left_list.sort()
+    right_list.sort()
+    total_distance = 0
+    for left, right in zip(left_list, right_list):
+        total_distance += abs(left - right)
+    print(f'Total distance: {total_distance}')
+
+def calculate_similarity_score(file_path):
+    left_list, right_list = read_and_split_numbers(file_path)
+    right_count = Counter(right_list)
+    similarity_score = 0
+    for number in left_list:
+        similarity_score += number * right_count[number]
+    print(f'Total similarity score: {similarity_score}')
+
+calculate_total_distance('day1\\input.txt')
+calculate_similarity_score('day1\\input.txt')
