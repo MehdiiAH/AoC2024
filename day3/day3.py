7739243afa4b9a34c4c11b42590a5ee60418ca32
MehdiiAH <mehdiait94@gmx.fr>
2024-12-03 13:58:31 +0100

    reformatting code

--- day3/day3.py
+++ day3/day3.py
@@ -1,12 +1,13 @@
 import re
 
+
 def extract_and_multiply_part1(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         content = file.read()
 
     total_sum = 0
 
-    instructions = re.findall(r'mul\((\d+),(\d+)\)', content)
+    instructions = re.findall(r"mul\((\d+),(\d+)\)", content)
 
     for instruction in instructions:
         x, y = map(int, instruction)
@@ -16,27 +17,28 @@ def extract_and_multiply_part1(file_path):
 
 
 def extract_and_multiply_part2(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         content = file.read()
 
     total_sum = 0
     mul_enabled = True
 
-    instructions = re.findall(r'do\(\)|don\'t\(\)|mul\(\d+,\d+\)', content)
+    instructions = re.findall(r"do\(\)|don\'t\(\)|mul\(\d+,\d+\)", content)
 
     for instruction in instructions:
-        if instruction == 'do()':
+        if instruction == "do()":
             mul_enabled = True
         elif instruction == "don't()":
             mul_enabled = False
         else:
             if mul_enabled:
-                num1, num2 = map(int, re.findall(r'\d+', instruction))
+                num1, num2 = map(int, re.findall(r"\d+", instruction))
                 total_sum += num1 * num2
 
     return total_sum
 
-file_path = 'input.txt'
+
+file_path = "input.txt"
 result1 = extract_and_multiply_part1(file_path)
 print(result1)
 
